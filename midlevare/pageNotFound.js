c56f9547528359e0b570fcb2f1dab54321d1e3c2
Andrej Kudinov <kudinov.andrej@list.ru>
2023-06-03 23:45:18 +0300

    второе ревью

--- midlevare/pageNotFound.js
+++ midlevare/pageNotFound.js
@@ -1,13 +1,14 @@
 const http2 = require('http2');
 
 const { HTTP_STATUS_NOT_FOUND } = http2.constants;
-function pageNotFound(req, res) {
+function pageNotFound(req, res, next) {
   const err = new Error('DocumentNotFoundError');
   if (err.message === 'DocumentNotFoundError') {
     res.status(HTTP_STATUS_NOT_FOUND).send({
       message: 'Страница не найдена',
     });
   }
+  next();
 }
 
 module.exports = pageNotFound;
