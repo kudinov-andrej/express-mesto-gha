241ed1131a521ded65fb01fa530c983e64499c05
Andrej Kudinov <kudinov.andrej@list.ru>
2023-06-03 18:55:17 +0300

    добавил управление безопасностью

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const {
   errors,
 } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes');
 const pageNotFound = require('./midlevare/pageNotFound');
 const ErrorHandler = require('./midlevare/ErrorHandler');
@@ -11,12 +13,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // за 15 минут
+  max: 100, // можно совершить максимум 100 запросов с одного IP
+});
+
 app.use(express.json());
+app.use(helmet());
+app.use(limiter);
 app.use(router);
 app.use(errors());
 app.use(ErrorHandler);
 app.use(pageNotFound);
 
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Приложение слушает на 3000 порту');
 });
